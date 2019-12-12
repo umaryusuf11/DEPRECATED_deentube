9d02181a2fc42e65eb1e0a894aa11e2b86d1cbe8
Umar Farouk Yusuf <umaryusuf11@gmail.com>
2019-12-12 23:27:58 +0100

    Reupload
    Bring it up to date with my Local Memory

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # deentube
+
+to do:
+    -o watch desktop screen:
+        -o fix css on morevideos 1 for title overflow
+        -o complete ejs listing for more videos 1
+        -o complete server.js get & response for the other more videos (4 found by tag and last one by random)
+        -o ejs listing for other more videos
+        -o window.alert for channel or video not active
+        -o create ad space
+        -o connect deentube logo to homepage
+        -o Disable Hamburger Icon
+        -o setup 404
+
+    -o Create Homepage
+
+    -o Create Content Creator Dashboard
+
+    -o Setup Ads
+
+    -o Remake Site for Mobile
+
+    -o Work out Payment System for Content Creators
+
+    -o Source Funding
+
+    -o Populate Video Base with Open Source and DeenTube Content
+
+    -o Reach Out to Content Creators
+
+    -o Advertise
+
+    -o Expand
--- server.js
+++ server.js
@@ -18,37 +18,68 @@ app.get("/watch/:vid" , (req,res) => {
 
         var currentVideo;
         var currentChannel;
+        var MoreVids1 = {};
         var JSONREPLY;
         //Get Current Video Details
             getVideo(req.params.vid,(vidResp) =>{
                 currentVideo = vidResp;
 
+
                     //Get Current Channel Details
                     getChannel(currentVideo.channelID, (channelResp) =>{
                         currentChannel = channelResp;
 
+                        var similarVideos;
+                        getVideosWithTag("a",5,(results)=>{
+                            similarVideos = results;
+                            console.log(similarVideos);
+        
+                            getVideo(similarVideos[0], (vidResp) => {
+                                
+                                getChannel(vidResp.channelID, (channelResp) => {
+                                    MoreVids1 = {
+                                        "thumbnailURL": vidResp.thumbnailURL,
+                                        "title": vidResp.title,
+                                        "viewCount": vidResp.title,
+                                        "channelName":channelResp.channelName
+                                    }
+                                    console.log(MoreVids1);
+                                                            //Sets Reply Params
+                                    JSONREPLY={
+                                        //current video Details
+                                        "title":currentVideo.title,
+                                        "channelID": currentVideo.channelID,
+                                        "videoViewCount":currentVideo.viewCount,
+                                        "description":currentVideo.description,
+                                        "videoURL":currentVideo.videoURL,
+                                        "thumbnailURL":currentVideo.thumbnailURL,
+                                        "tags":currentVideo.tags,
+                                        "isVideoActive":currentVideo.isVidActive,
 
-                        //Sets Reply Params
-                        JSONREPLY={
-                            //current video Details
-                            "title":currentVideo.title,
-                            "channelID": currentVideo.channelID,
-                            "videoViewCount":currentVideo.viewCount,
-                            "description":currentVideo.description,
-                            "videoURL":currentVideo.videoURL,
-                            "thumbnailURL":currentVideo.thumbnailURL,
-                            "tags":currentVideo.tags,
-                            "isVideoActive":currentVideo.isVidActive,
+                                        //current Channel Details
+                                        "channelName":currentChannel.channelName,
+                                        "channelIconURL":currentChannel.channelIconURL,
+                                        "isChannelActive":currentChannel.isChannelActive,
+                                        "channelViewCount":currentChannel.viewCount,
+                                        "videos":currentChannel.videos,
 
-                            //current Channel Details
-                            "channelName":currentChannel.channelName,
-                            "channelIconURL":currentChannel.channelIconURL,
-                            "isChannelActive":currentChannel.isChannelActive,
-                            "channelViewCount":currentChannel.viewCount,
-                            "videos":currentChannel.videos
-                        }
-                        //Renders Page
-                        res.render('watch',JSONREPLY);
+                                        //more videos
+                                        //more videos 1
+                                        "moreVids1ThumbnailURL":MoreVids1.thumbnailURL,
+                                        "moreVids1Title":MoreVids1.title,
+                                        "moreVids1ViewCount":MoreVids1.viewCount,
+                                        "moreVids1ChannelName": MoreVids1.channelName
+                                    }
+                                    //Renders Page
+                                    res.render('watch',JSONREPLY);
+                                });
+            
+            
+                            });
+        
+                        });
+                            
+
                     })
 
 
@@ -89,6 +120,70 @@ function getChannel(id,callback){
         callback(RequestedChannel);
     })
 }
+
+function getVideosWithTag(tags,number,callback){
+    readJSON("static/db/videos.json", (res) =>{
+
+        var results = [];
+        var searchField = "tags";
+        var searchVal;
+        ////////////////////////////////////////////////////////////////
+        //for each tag in tags passed into function
+        for(i=0; i<tags.length;i++){
+            searchVal = tags[i];
+
+            //for each item in res object
+            for(var x in res){
+                //for each tag in item in res object
+                for(var i=0 ; i < res[x].tags.length ; i++){
+                    //if tag = searVal
+                    if (res[x][searchField] == searchVal) {
+                        //Add item to results array
+                        results.push(x);
+                        console.log(results.length)
+                        }
+                }
+            } 
+    
+        }
+
+        //if number was passed
+        if(number != null && number != "" && number != undefined ){
+            while(results.length != number){
+                if(results.length<number){
+                    var length = results.length;
+                    for(i=0; i<length; i++){
+                        var add = results[i];
+                        results.push(add);
+                    }
+    
+                    if(results.length>number){
+                        while(results.length>number){
+                            results.shift();
+                        }
+                    }
+    
+                }
+    
+                //if results is larger than number requested remove all the extras starting from the top (oldest in db)
+                if(results.length>number){
+                    while(results.length>number){
+                        results.shift();
+                    }
+                }
+
+            }
+        }
+
+        
+        callback(results);
+
+
+
+    })
+
+}
+
 
 //Start Server
 app.listen(Port, ()=> console.log('Server is Started on Port: '+Port));
